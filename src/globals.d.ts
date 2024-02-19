7cb48db9835c58e4273c5cd295e38e59b48fd2e9
Michal99Zajac <michal.99.zajac@gmail.com>
2024-02-19 11:00:42 +0100

    feat: add zod global type

---
+++ src/globals.d.ts
@@ -0,0 +1,20 @@
+import {
+  FastifyBaseLogger,
+  FastifyInstance,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerDefault,
+} from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+declare global {
+  type FastifyZodInstance = FastifyInstance<
+    RawServerDefault,
+    RawRequestDefaultExpression<RawServerDefault>,
+    RawReplyDefaultExpression<RawServerDefault>,
+    FastifyBaseLogger,
+    ZodTypeProvider
+  >
+}
+
+export {}
