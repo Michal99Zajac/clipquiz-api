6740a8fad70b1fd18332226e650d7904f55bd314
Michal99Zajac <michal.99.zajac@gmail.com>
2024-03-04 21:57:17 +0100

    feat: remove some default values from env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,11 +5,11 @@ export const env = cleanEnv(process.env, {
   PORT: port({ default: 8080 }),
   NODE_ENV: str({ choices: ['development', 'production', 'test'], default: 'development' }),
   // DATABASE
-  DATABASE_HOST: host({ default: 'localhost' }),
+  DATABASE_HOST: host(),
   DATABASE_PORT: port({ default: 5432 }),
-  DATABASE_USERNAME: str({ default: 'postgres' }),
-  DATABASE_PASSWORD: str({ default: 'postgres' }),
-  DATABASE_NAME: str({ default: 'postgres' }),
+  DATABASE_USERNAME: str(),
+  DATABASE_PASSWORD: str(),
+  DATABASE_NAME: str(),
   DATABASE_SSL: bool({ default: false }),
 })
 
