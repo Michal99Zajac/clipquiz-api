c1851281a7e49366393ac46b244ac8f605a75b3d
Michal99Zajac <michal.99.zajac@gmail.com>
2024-03-07 20:04:27 +0100

    build: return to pure dotenv

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['dotenv-flow/config'],
+  setupFiles: ['dotenv/config'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
