649c0adc474091f8235c78bdd4edf1dc14756609
Michal99Zajac <michal.99.zajac@gmail.com>
2024-03-07 18:48:02 +0100

    feat: use dotenv-flow instead of pure dotenv

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,22 @@ const fastify = Fastify({
 fastify.register(app)
 
 export default fastify
+
+/* -------------------------------------------------------------------------- */
+/*                                 DEV SERVER                                 */
+/* -------------------------------------------------------------------------- */
+
+if (require.main === module) {
+  fastify.ready((error) => {
+    if (error) fastify.log.error(error)
+
+    // Clear the console
+    fastify.log.info('\x1Bc')
+  })
+
+  // Start the dev server
+  fastify.listen({
+    host: env.HOST,
+    port: env.PORT,
+  })
+}
