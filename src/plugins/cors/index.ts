c8c0ded622a965ab836cb73054220bd7d48716ef
Michal99Zajac <michal.99.zajac@gmail.com>
2024-03-10 19:17:52 +0100

    feat(plugins): mount cors plugin

---
+++ src/plugins/cors/index.ts
@@ -0,0 +1,11 @@
+import cors from '@fastify/cors'
+import fp from 'fastify-plugin'
+
+/**
+ * This plugins adds some utilities to handle CORS
+ *
+ * @see https://github.com/fastify/fastify-cors
+ */
+export default fp(async (fastify) => {
+  await fastify.register(cors)
+})
